dd013e837c4341830d8260d0305ac6d80aa7dd34
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-03-09 21:29:07 -0500

    Add digitize function   - A function that convert number to a reversed array of digits

--- digitize.py
+++ digitize.py
@@ -4,3 +4,10 @@
 # You have to return the digits of this number within an array in reverse order.
 #
 # Example: 348597 => [7,9,5,8,4,3]
+
+def digitize(n):
+    n = str(n)[::-1]
+    answer = []
+    for x in n:
+        answer.append(int(x))
+    return answer

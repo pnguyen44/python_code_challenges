2b1ac222ef38a077cb9ca553edcbb4f38ceb6431
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-03-14 23:38:50 -0400

    Add solution

--- accum.py
+++ accum.py
@@ -7,3 +7,13 @@
 # accum("RqaEzty") # "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 # accum("cwAt")    # "C-Ww-Aaa-Tttt"
 # The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+def accum(s):
+    result = []
+    s = list(s)
+    s = [' '] + s
+    for x  in range(len(s)):
+        result.append((s[x] * x).title())
+    result.pop(0)
+
+    return '-'.join(result)

ea329cc093feedda64a6ae4714d51b07c660f8b5
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-03-14 22:06:44 -0400

    Add alternative solution

--- bin_to_decimal.py
+++ bin_to_decimal.py
@@ -3,3 +3,11 @@
 
 def bin_to_decimal(inp):
     return int(inp,2)
+
+# Alternative Solution:
+# def bin_to_decimal(inp):
+#     result = 0
+#     inp =  inp[:: -1]
+#     for i in range(len(inp)):
+#         result += int(inp[i]) * 2 ** i
+#     return result

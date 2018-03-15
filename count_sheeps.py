88d541f432c4aacbfebd7e09da943516a6550060
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-03-15 01:41:44 -0400

    Add solution to code challenge

--- count_sheeps.py
+++ count_sheeps.py
@@ -13,3 +13,10 @@
 # The correct answer would be 17.
 #
 # Hint: Don't forget to check for bad values like null/undefined
+
+def count_sheeps(arrayOfSheeps):
+    count = 0
+    for x  in arrayOfSheeps:
+        if x == True:
+            count += 1
+    return count

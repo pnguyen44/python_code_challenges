a713c5fb2aea8fdb83f5db40d9561e27f1485ebd
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-03-15 23:51:01 -0400

    Add alternative solutions

--- disemvowel.py
+++ disemvowel.py
@@ -12,3 +12,12 @@
 import re
 def disemvowel(string):
     return re.sub('[aeiou]', '', string , flags = re.IGNORECASE )
+
+
+# Alternative Solutions:
+#
+# def disemvowel(s):
+#     return s.translate(None, "aeiouAEIOU")
+#
+# def disemvowel(string):
+#     return "".join(c for c in string if c.lower() not in "aeiou")
